a2d76f182dcce3286a3283839025a1dff8b4a886
asmit27rai <raiasmit10@gmail.com>
2024-09-14 20:14:33 +0530

    Event Added

--- frontend/src/components/LandingPages/Events/Events_Components.js
+++ frontend/src/components/LandingPages/Events/Events_Components.js
@@ -104,10 +104,16 @@ const EventCard = (props) => {
 const PersonDetails = (props) => {
   return (
     <div className={`${styles.person_details}`}>
-    <br />
-      <span className={`${styles.content_header}`}>{props.name}<br /></span>
-      <span className={`${styles.content}`}>{props.contact}<br /></span>
-      <span className={`${styles.content}`}>{props.email}<br /><br /></span>
+      <br />
+      <span className={`${styles.content_header}`} style={{ color: 'white' }}>
+        {props.name}<br />
+      </span>
+      <span className={`${styles.content}`} style={{ color: 'white' }}>
+        {props.contact}<br />
+      </span>
+      <span className={`${styles.content}`} style={{ color: 'white' }}>
+        {props.email}<br /><br />
+      </span>
     </div>
   );
 };
