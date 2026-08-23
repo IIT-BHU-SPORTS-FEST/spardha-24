83a4dc6ab4b18fa73be9faf2f4f55afc14156951
weirdo-0-1 <mokshithcodes@gmail.com>
2026-08-23 15:05:26 +0530

    new update

--- frontend/src/components/LandingPages/Home/Countdown/Countdown.js
+++ frontend/src/components/LandingPages/Home/Countdown/Countdown.js
@@ -91,7 +91,7 @@ class CountdownTimer extends Component {
     super(props);
 
     // Correct IST conversion: add 5 hours 30 minutes to UTC
-     const utcDate = new Date("2026-10-10T00:00:00Z");
+     const utcDate = new Date("2025-10-10T00:00:00Z");
     this.targetDate = new Date(utcDate.getTime() + (5.5 * 60 * 60 * 1000));
 
     this.state = this.calculateTimeDifference();
