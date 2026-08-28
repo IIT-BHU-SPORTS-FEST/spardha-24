2e40b7551033b50adc75c782d56c62f2d20e62b1
shaurya <shauryadeep776@gmail.com>
2026-08-28 23:24:00 +0530

    Fix footer logo image path
    B

--- frontend/src/components/LandingPages/Footer/footer.js
+++ frontend/src/components/LandingPages/Footer/footer.js
@@ -17,7 +17,11 @@ const Footer = () => {
           {/* Left Column */}
           <div className={css['footer-left']}>
             <img
+<<<<<<< HEAD
               src='./images/logo/spardha26.jpeg'
+=======
+              src="/images/logo/spardha26.jpeg
+>>>>>>> 63a80f8 (Fix footer logo image path)
               className={css['footer-logo']}
               alt="Spardha IIT BHU Logo"
             />
